f248c49103222c5d684f77f99916cee939ab1763
zeromH <cupdisin@gmail.com>
2021-02-18 00:48:04 +0900

    Guardians Add, IActionResult Practice

---
+++ Welfare/Controllers/GuardiansController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Welfare.Controllers
+{
+    public class GuardiansController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Details(int id)
+        {
+            ViewData["Id"] = id;
+
+
+            return View();
+        }
+    }
+}
---
+++ Welfare/Models/GuardianViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Welfare.Models
+{
+    public class GuardianViewModel : ModelBase
+    {
+        public string Name { get; set; }
+    }
+}
