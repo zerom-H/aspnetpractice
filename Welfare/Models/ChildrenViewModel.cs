06ce0bbd5375a25542f52d6fd7d4d09be175a7d0
zeromH <cupdisin@gmail.com>
2021-02-18 00:21:50 +0900

    First Commit

---
+++ Welfare/Models/ChildrenViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Welfare.Models
+{
+    public class ChildrenViewModel : ModelBase
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ Welfare/Models/ModelBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Welfare.Models
+{
+    public abstract class ModelBase
+    {
+        public int Id { get; set; }
+
+        public bool Active { get; set; }
+        
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [StringLength(100)]
+        public string CreateBy { get; set; }
+    }
+}
